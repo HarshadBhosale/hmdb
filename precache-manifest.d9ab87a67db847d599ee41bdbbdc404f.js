2a6666cc1558e5ff1eb53806975990f6eba3c5a0
HarshadBhosale <harshad.bhosale@cogoport.com>
2023-02-20 17:39:53 +0530

    Updates

---
+++ precache-manifest.d9ab87a67db847d599ee41bdbbdc404f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4258dea08563f13fa004",
+    "url": "/hmdb/static/css/main.936cc987.chunk.css"
+  },
+  {
+    "revision": "4258dea08563f13fa004",
+    "url": "/hmdb/static/js/main.4258dea0.chunk.js"
+  },
+  {
+    "revision": "8b656c464ee787dcef01",
+    "url": "/hmdb/static/js/1.8b656c46.chunk.js"
+  },
+  {
+    "revision": "000ac1d90a35f5dd3fb5",
+    "url": "/hmdb/static/js/runtime~main.000ac1d9.js"
+  },
+  {
+    "revision": "d33f01a4f77b2d04b1ba88d8faed1d7e",
+    "url": "/hmdb/index.html"
+  }
+];
